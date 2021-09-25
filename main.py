43faee83f358554a06ea467fbdf0772cfabfc3a9
Vuuuuu <43222738+Vuuuuu@users.noreply.github.com>
2021-09-25 13:20:32 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,69 @@
-import paho.mqtt.client as mqtt
-import time
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
-MQTT_ADDRESS = '172.20.10.12'
-MQTT_PORT = 1883
-MQTT_USER = 'johan'
-MQTT_PASSWORD = 'test123'
-MQTT_TOPIC = 'home/+/+'
+root = tk.Tk()
 
-print("test1")
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+canvas1 = tk.Canvas(root, width=800, height=300)
+canvas1.pack()
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe(MQTT_TOPIC)
+label1 = tk.Label(root, text='Graphical User Interface')
+label1.config(font=('Arial', 20))
+canvas1.create_window(400, 50, window=label1)
 
-print("test2")
+entry1 = tk.Entry(root)
+canvas1.create_window(400, 100, window=entry1)
+
+entry2 = tk.Entry(root)
+canvas1.create_window(400, 120, window=entry2)
+
+entry3 = tk.Entry(root)
+canvas1.create_window(400, 140, window=entry3)
+
+def create_charts():
+    global x1
+    global x2
+    global x3
+    global bar1
+    global pie2
+    x1 = float(entry1.get())
+    x2 = float(entry2.get())
+    x3 = float(entry3.get())
+
+    figure1 = Figure(figsize=(4, 3), dpi=100)
+    subplot1 = figure1.add_subplot(111)
+    xAxis = [float(x1), float(x2), float(x3)]
+    yAxis = [float(x1), float(x2), float(x3)]
+    subplot1.bar(xAxis, yAxis, color='lightsteelblue')
+    bar1 = FigureCanvasTkAgg(figure1, root)
+    bar1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=0)
+
+    figure2 = Figure(figsize=(4, 3), dpi=100)
+    subplot2 = figure2.add_subplot(111)
+    labels2 = 'Label1', 'Label2', 'Label3'
+    pieSizes = [float(x1), float(x2), float(x3)]
+    my_colors2 = ['lightblue', 'lightsteelblue', 'silver']
+    explode2 = (0, 0.1, 0)
+    subplot2.pie(pieSizes, colors=my_colors2, explode=explode2, labels=labels2, autopct='%1.1f%%', shadow=True,
+                 startangle=90)
+    subplot2.axis('equal')
+    pie2 = FigureCanvasTkAgg(figure2, root)
+    pie2.get_tk_widget().pack()
 
 
-def on_message(client, userdata, message):
-    print("Message recieved: " + message.topic + ' ' + str(message.payload))
-
-print("test3")
-
-def main():
-    mqtt_client = mqtt.Client()
-    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
-    mqtt_client.on_connect = on_connect
-    mqtt_client.on_message = on_message
-
-    mqtt_client.connect(MQTT_ADDRESS, 1883)
-    mqtt_client.loop_forever()
+def clear_charts():
+    bar1.get_tk_widget().pack_forget()
+    pie2.get_tk_widget().pack_forget()
 
 
-if __name__ == '__main__':
-    print('test4')
-    main()
+button1 = tk.Button(root, text=' Create Charts ', command=create_charts, bg='palegreen2', font=('Arial', 11, 'bold'))
+canvas1.create_window(400, 180, window=button1)
+
+button2 = tk.Button(root, text='  Clear Charts  ', command=clear_charts, bg='lightskyblue2', font=('Arial', 11, 'bold'))
+canvas1.create_window(400, 220, window=button2)
+
+button3 = tk.Button(root, text='Exit Application', command=root.destroy, bg='lightsteelblue2',
+                    font=('Arial', 11, 'bold'))
+canvas1.create_window(400, 260, window=button3)
+
+root.mainloop()
